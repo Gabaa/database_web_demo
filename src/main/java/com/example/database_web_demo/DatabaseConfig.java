db06070c2e4f5b43d50110d3c4c6dc0c42809c6e
Mikkel Gaba <au615597@uni.au.dk>
2019-12-30 13:50:49 +0100

    Replaced Value annotation usage with Environment

--- src/main/java/com/example/database_web_demo/DatabaseConfig.java
+++ src/main/java/com/example/database_web_demo/DatabaseConfig.java
@@ -2,22 +2,34 @@ package com.example.database_web_demo;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
+    private final Environment env;
+
+    public DatabaseConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
+
+        String dbUrl = env.getProperty("spring.datasource.url");
         config.setJdbcUrl(dbUrl);
+
+        String dbUsername = env.getProperty("spring.datasource.username");
+        config.setUsername(dbUsername);
+
+        String dbPassword = env.getProperty("spring.datasource.password");
+        config.setPassword(dbPassword);
+
         return new HikariDataSource(config);
     }
 }
